a7c73e01f25f0cc23d82e866161db933cbb4e82a
kamesh63 <146455043+kamesh63@users.noreply.github.com>
2025-06-13 10:28:59 +0530

    Update programs.py

--- programs.py
+++ programs.py
@@ -10,7 +10,7 @@ b = c
 print(f"After swap:\na:{a}\tb:{b}")
 
 # Program 2
-# Swap two numbers - Using maths
+# Swap two numbers - Using + and -
 
 a = int(input("A: ")) # a = 10
 b = int(input("B: ")) # b = 20
@@ -18,4 +18,15 @@ print(f"Before swap:\na:{a}\tb:{b}")
 a = a + b # a = 10 + 20 = 30
 b = a - b # b = 30 - 20 = 10
 a = a - b # a = 30 - 10 = 20
+print(f"After swap:\na:{a}\tb:{b}")
+
+# Program 3
+# Swap two numbers - Using * and floor division (//)
+
+a = int(input("A: ")) # a = 10
+b = int(input("B: ")) # b = 20
+print(f"Before swap:\na:{a}\tb:{b}")
+a = a + b 
+b = a // b 
+a = a // b 
 print(f"After swap:\na:{a}\tb:{b}")
